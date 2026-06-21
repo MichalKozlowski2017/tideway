2f6ee6bcaafd8bc5dc4e19cdf0944c1521dba616
MichalKozlowski2017 <m.kozlowski87@gmail.com>
2026-06-21 22:39:11 +0200

    Redirect .eu and www domains to canonical tideway.pl

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,17 @@
 import type { NextConfig } from "next";
+import { PRIMARY_SITE_URL } from "./lib/site";
+
+const secondaryHosts = ["tideway.eu", "www.tideway.eu", "www.tideway.pl"];
 
 const nextConfig: NextConfig = {
+  async redirects() {
+    return secondaryHosts.map((host) => ({
+      source: "/:path*",
+      has: [{ type: "host", value: host }],
+      destination: `${PRIMARY_SITE_URL}/:path*`,
+      permanent: true,
+    }));
+  },
   images: {
     remotePatterns: [
       { protocol: "https", hostname: "**" },
